4898c09f305d33a2ea3011cd9b30987602561ffb
unknown <rogerzc@edu.unisinos.br>
2021-09-23 10:03:51 -0300

    Adicionada o usuário no topo da página

--- partials/user.php
+++ partials/user.php
@@ -1,4 +1,5 @@
 <div class="container justify-content-center text-center">
+    <p><h2>Olá, <?php echo $_SESSION['usuario']; ?></h2></p>
     <span>
         <h2><a class="btn btn-danger col-md-4" href="../logout.php">Sair</a></h2>
     </span>
